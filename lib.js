c90fbceed79b401bd3ac03a3557d45df8a3acc15
i-do-ai <121050605+i-do-ai@users.noreply.github.com>
2022-12-19 19:41:58 +0200

    Update lib.js

--- lib.js
+++ lib.js
@@ -1,6 +1,10 @@
 function Perceptron(options) {
-if (!options || !options.learningRate || !options.bias || !options.inputsLength || !options.weights || !options.threshold) throw new Error('Please fill out the options correctly!');
-
+if (!options || !options.inputsLength) throw new Error('Network options not filled correctly!');
+if (!options.threshold) options.threshold = options.weights / 3 / 10;
+if (!options.bias) options.bias = 1;
+if (!options.learningRate) options.learningRate = 0.0001;
+if (!options.weights) { options.weights = []; for (let i = 0; i < options.inputsLength; i++) options.weights.push(Math.random()); };
+  
 this.run = function(inputs) {
   if (inputs.length != options.weights.length) throw new Error('Unexpected amount of inputs!');
   let sum = 0;
